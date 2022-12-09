954538a5bf306cfb3b3c19fa52798cc76ee1ddaa
PRASHANT SINGH <ps.prashantsingh.98@gmail.com>
2022-12-09 18:35:46 +0530

    All component changed to functional  component

--- src/hoc/adminLayout.js
+++ src/hoc/adminLayout.js
@@ -1,71 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Header from './../common/header';
 import Sidebar from './../common/sidebar';
 import { Preloader, Bars } from 'react-preloader-icon';
 
 const adminLayout = (ChildComponent) => {
-    class AdminLayout extends React.Component {
-        constructor(props){
-            super(props);
-    
-            this.state = {
-                pageLoaded: false,
-                saveLeadClickEvent: ""
-            };
-        }
+  const AdminLayout = (props) => {
+    const [pageLoaded, setpageLoaded] = useState(false);
+    const [saveLeadClickEvent, setsaveLeadClickEvent] = useState('');
 
-        componentDidMount(){
-            setTimeout(() => {
-                this.setState(() => ({
-                    pageLoaded: true
-                }))
-            }, 1000);
-        }
+    useEffect(() => {
+      setTimeout(() => {
+        setpageLoaded(() => true);
+      }, 1000);
 
-        renderHtml(){
-            if(!this.state.pageLoaded){
-                return <div className="loading-page">
-                    <div className="center">
-                        <Preloader use={Bars} size={60} strokeWidth={10} strokeColor="#f7b085" duration={600} />
-                    </div>
-              </div>
-            }
+      return () => {};
+    }, []);
 
-            return <div className="d-flex" id="wrapper">
-                {/* <!-- Sidebar--> */}
-                <Sidebar/>
-                {/* <!-- Page content wrapper--> */}
-                <div className="main" id="page-content-wrapper">
-                    {/* <!-- Top navigation--> */}
-                    <Header />
-                    {/* <!-- Page content--> */}
-                    <div className="container-fluid content-container">
-                        <ChildComponent {...this.props} />
-                    </div>
-                </div>
+    const renderHtml = () => {
+      if (!pageLoaded) {
+        return (
+          <div className="loading-page">
+            <div className="center">
+              <Preloader
+                use={Bars}
+                size={60}
+                strokeWidth={10}
+                strokeColor="#f7b085"
+                duration={600}
+              />
             </div>
-        }
+          </div>
+        );
+      }
 
-        addLeadModalFooterContent(){ 
-            return <>
-                <div style={{width:"100%"}}>
-                    <button onClick={(e) => this.setState(() => ({saveLeadClickEvent: (Math.random() + 1).toString(36).substring(7)}))} className="btn btn-default low-height-btn">Add Lead</button> 
-                </div>
-            </>;
-        }
+      return (
+        <div className="d-flex" id="wrapper">
+          {/* <!-- Sidebar--> */}
+          <Sidebar />
+          {/* <!-- Page content wrapper--> */}
+          <div className="main" id="page-content-wrapper">
+            {/* <!-- Top navigation--> */}
+            <Header />
+            {/* <!-- Page content--> */}
+            <div className="container-fluid content-container">
+              <ChildComponent {...props} />
+            </div>
+          </div>
+        </div>
+      );
+    };
 
-        handleParentData = (e) => {
-            console.log(e);
-        }
+    const addLeadModalFooterContent = () => (
+      <>
+        <div style={{ width: '100%' }}>
+          <button
+            onClick={(e) =>
+              setsaveLeadClickEvent(
+                (Math.random() + 1).toString(36).substring(7)
+              )
+            }
+            className="btn btn-default low-height-btn"
+          >
+            Add Lead
+          </button>
+        </div>
+      </>
+    );
 
-        render(){
-            return <>
-                {this.renderHtml()}
-            </>
-        }
-    }
+    const handleParentData = (e) => {
+      console.log(e);
+    };
 
-    return AdminLayout;
-}
+    return <>{renderHtml()}</>;
+  };
+
+  return AdminLayout;
+};
 
 export default adminLayout;
